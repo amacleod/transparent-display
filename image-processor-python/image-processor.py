d68b1b6a50fa6addb685e9b6ebac0c9b367e277f
captainremy <remyxbourbeau@gmail.com>
2025-02-23 14:43:15 -0500

    now converts to PNG

--- image-processor-python/image-processor.py
+++ image-processor-python/image-processor.py
@@ -3,8 +3,21 @@ image-processor - the main image processor program, to be run on the host PC.
 
 """
 
-def main():
-    print("Hello, world!")
+import os, sys
+from PIL import Image
 
+def main():
+    for infile in sys.argv[1:]:
+        f, e = os.path.splitext(infile)
+        outfile = f + ".png"
+        if infile != outfile:
+            try:
+                with Image.open(infile) as im:
+                    im.save(outfile)
+                    im.show()
+            except OSError:
+                print("cannot convert", infile)
+        else:
+            print("not converting because identical")
 if __name__ == "__main__":
     main()
