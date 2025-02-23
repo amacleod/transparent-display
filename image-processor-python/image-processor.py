4c2bc3cc20d58ebbeb9812b1e5341b59b783db2a
captainremy <remyxbourbeau@gmail.com>
2025-02-23 14:59:56 -0500

    sets image to grayscale

--- image-processor-python/image-processor.py
+++ image-processor-python/image-processor.py
@@ -5,19 +5,23 @@ image-processor - the main image processor program, to be run on the host PC.
 
 import os, sys
 from PIL import Image
+imageFileName = "../MirrahRatio.jpg"
 
 def main():
-    for infile in sys.argv[1:]:
-        f, e = os.path.splitext(infile)
-        outfile = f + ".png"
-        if infile != outfile:
-            try:
-                with Image.open(infile) as im:
-                    im.save(outfile)
-                    im.show()
-            except OSError:
-                print("cannot convert", infile)
-        else:
-            print("not converting because identical")
+    # for infile in sys.argv[1:]:
+    #     f, e = os.path.splitext(infile)
+    #     outfile = f + ".png"
+    #     if infile != outfile:
+    #         try:
+    #             with Image.open(infile) as im:
+    #                 im.save(outfile)
+    #                 im.show()
+    #         except OSError:
+    #             print("cannot convert", infile)
+    #     else:
+    #         print("not converting because identical")
+    with Image.open(imageFileName) as im:
+        im_luminance = im.convert("L")
+        im_luminance.show()
 if __name__ == "__main__":
     main()
