4102175d443ce4939633784826e724b013cdba8f
Allister MacLeod <allister.macleod@gmail.com>
2025-03-01 13:33:21 -0500

    Introducing constants for width and height, so that we can do math on them and change them in one place if needed.

--- image-processor-python/image-processor.py
+++ image-processor-python/image-processor.py
@@ -8,8 +8,14 @@ import logging as log
 import PIL.Image
 import PIL.ImageOps
 
+DISPLAY_WIDTH = 128
+DISPLAY_HEIGHT = 56
+DISPLAY_HALF_WIDTH = DISPLAY_WIDTH / 2
+DISPLAY_HALF_HEIGHT = DISPLAY_HEIGHT / 2
+
 log.basicConfig(level=log.INFO, format="%(asctime)s %(levelname)-8s %(message)s")
 
+# TODO: Eventually we want the file to come from outside. ~ACM 2025-03-01
 imageFileName = "../MirrahRatio.jpg"
 # set SHOW_IMAGES to False to prevent image popup debugging
 SHOW_IMAGES = True
@@ -21,10 +27,15 @@ def main():
     width, height = input_image.size
     log.info(f"Image dimensions: {width}px wide x {height}px high")
     im_black_and_white = input_image.convert("1")
-    im_padded = PIL.ImageOps.pad(im_black_and_white, (128, 56), color="#000")
-    center_x = 205 / 2
-    center_y = 246 / 2
-    box = (center_x - 64, center_y - 28, center_x + 64, center_y + 28)
+    im_padded = PIL.ImageOps.pad(im_black_and_white, (DISPLAY_WIDTH, DISPLAY_HEIGHT), color="#000")
+    center_x = width / 2
+    center_y = height / 2
+    box = (
+        center_x - DISPLAY_HALF_WIDTH,
+        center_y - DISPLAY_HALF_HEIGHT,
+        center_x + DISPLAY_HALF_WIDTH,
+        center_y + DISPLAY_HALF_HEIGHT
+    )
     im_black_and_white_crop = im_black_and_white.crop(box)
     if SHOW_IMAGES:
         im_black_and_white_crop.show("black and white")
