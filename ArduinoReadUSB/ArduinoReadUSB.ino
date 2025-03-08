9d8b27d6757702e5700fbd6d35ed7ce2b4d354cf
Allister MacLeod <allister.macleod@gmail.com>
2025-03-08 18:18:14 -0500

    Working echo logic for Python to Arduino communication.

--- ArduinoReadUSB/ArduinoReadUSB.ino
+++ ArduinoReadUSB/ArduinoReadUSB.ino
@@ -1,61 +1,17 @@
-
-
-
-const byte numChars = 5;
-char receivedChars[numChars];
-
-boolean newData = false;
-
 void setup() {
     Serial.begin(9600);
-    Serial.println("<Arduino is ready>");
+    Serial.println("Arduino is ready");
 }
 
 void loop() {
-    recvWithStartEndMarkers();
-    showNewData();
+  while (!Serial.available()) {
+    delay(100);
+  }
+  read_and_echo();
 }
 
-void recvWithStartEndMarkers() {
-    static boolean recvInProgress = false;
-    static byte ndx = 0;
-    char startMarker = '<';
-    char endMarker = '>';
-    char rc;
- 
- // if (Serial.available() > 0) {
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
-
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= numChars) {
-                    ndx = numChars - 1;
-                }
-            }
-            else {
-                receivedChars[ndx] = '\0'; // terminate the string
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
-            }
-        }
-
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
-    }
+void read_and_echo() {
+  String s = Serial.readStringUntil("\n");
+  String response = s.substring(0, s.length() - 1);
+  Serial.println(response);
 }
-
-void showNewData() {
-    if (newData == true) {
-        Serial.print("This just in ... ");
-        Serial.println(receivedChars);
-        newData = false;
-    }
-}
-
-
-//================================================================================
--- image-processor-python/serial-writer.py
+++ image-processor-python/serial-writer.py
@@ -2,35 +2,37 @@
 serial-writer.py - proof of concept for Python writing to USB serial port.
 
 """
-import time
 
+import time
 import serial
 
 import logging as log
 log.basicConfig(level=log.DEBUG, format="%(asctime)s %(levelname)-8s %(message)s")
 
 COM_PORT = "COM30"
-BUFFER_LENGTH = 16
-SLEEP_INTERVAL = 0.05
+SLEEP_INTERVAL = 1.0
 READ_TIMEOUT = 0.5
 
 def main():
     ser = serial.Serial(COM_PORT, timeout=READ_TIMEOUT)
     log.info(f"Serial port opened: {ser.name}, {ser.baudrate}, {ser.bytesize}, {ser.parity}, {ser.timeout}")
-    bytes_to_write = b"hello"
+    bytes_to_write = b"hello\n"
+    ready = False
     while True:
         try:
-            log.debug(f"Writing '{bytes_to_write}'")
-            write_result = ser.write(bytes_to_write)
-            log.debug(f"Write result: {write_result}")
-            response = ser.read(BUFFER_LENGTH)
-            log.debug(response)
-            if len(response) < BUFFER_LENGTH:
-                time.sleep(SLEEP_INTERVAL)
-            if bytes_to_write == b"hello":
-                bytes_to_write = b"later"
-            else:
-                bytes_to_write = b"hello"
+            response = ser.read_until(b"\r\n")[:-2].decode("utf-8")
+            log.debug(f"From Arduino: '{response}'")
+            if "ready" in response:
+                ready = True
+            if ready:
+                log.debug(f"Writing: {bytes_to_write}")
+                bytes_written = ser.write(bytes_to_write)
+                log.debug(f"Wrote {bytes_written} bytes.")
+                if bytes_to_write == b"hello\n":
+                    bytes_to_write = b"goodbye\n"
+                else:
+                    bytes_to_write = b"hello\n"
+            time.sleep(SLEEP_INTERVAL)
         except KeyboardInterrupt:
             log.debug("Interrupted.")
             break
