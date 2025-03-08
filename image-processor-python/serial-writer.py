b97a84f092b3c860d03b92e0056fdb65e901ea05
Allister MacLeod <allister.macleod@gmail.com>
2025-03-08 17:33:04 -0500

    Sending "hello" and "later" to serial port.

--- image-processor-python/serial-writer.py
+++ image-processor-python/serial-writer.py
@@ -12,21 +12,28 @@ log.basicConfig(level=log.DEBUG, format="%(asctime)s %(levelname)-8s %(message)s
 COM_PORT = "COM30"
 BUFFER_LENGTH = 16
 SLEEP_INTERVAL = 0.05
+READ_TIMEOUT = 0.5
 
 def main():
-    ser = serial.Serial(COM_PORT, timeout=0.5)
+    ser = serial.Serial(COM_PORT, timeout=READ_TIMEOUT)
     log.info(f"Serial port opened: {ser.name}, {ser.baudrate}, {ser.bytesize}, {ser.parity}, {ser.timeout}")
-    ser.write(b"hello")
-    timed_out = False
+    bytes_to_write = b"hello"
     while True:
-        response = ser.read(BUFFER_LENGTH)
-        log.debug(response)
-        if len(response) < BUFFER_LENGTH:
-            try:
+        try:
+            log.debug(f"Writing '{bytes_to_write}'")
+            write_result = ser.write(bytes_to_write)
+            log.debug(f"Write result: {write_result}")
+            response = ser.read(BUFFER_LENGTH)
+            log.debug(response)
+            if len(response) < BUFFER_LENGTH:
                 time.sleep(SLEEP_INTERVAL)
-            except KeyboardInterrupt:
-                log.debug("Interrupted.")
-                break
+            if bytes_to_write == b"hello":
+                bytes_to_write = b"later"
+            else:
+                bytes_to_write = b"hello"
+        except KeyboardInterrupt:
+            log.debug("Interrupted.")
+            break
     ser.close()
     log.info("Done.")
 
