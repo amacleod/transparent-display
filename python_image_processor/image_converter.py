51a0aeb093248f67d74a80d71aa143b3acd19655
Allister MacLeod <allister.macleod@gmail.com>
2025-04-20 14:08:16 -0400

    Cleanup in image_converter.py
    Pythonicize variable names. PEP-8-ify line breaks and spacing.

--- python_image_processor/image_converter.py
+++ python_image_processor/image_converter.py
@@ -3,11 +3,14 @@ image_converter.py - proof of concept for Python sending loaded image via USB se
 
 """
 
-import numpy
-# import binascii
-import PIL.Image
 import logging as log
+
+import PIL.Image
+import numpy
+
 log.basicConfig(level=log.DEBUG, format="%(asctime)s %(levelname)-8s %(message)s")
+
+DEBUG_EVERY_BYTE = False
 
 
 def bytes_from_file(filename: str) -> bytes:
@@ -15,28 +18,29 @@ def bytes_from_file(filename: str) -> bytes:
     return image_to_bytes(image)
 
 
-def image_to_bytes(input_image :PIL.Image) -> bytes:
+def image_to_bytes(input_image: PIL.Image) -> bytes:
     array = numpy.asarray(input_image).astype(int)
     return array_to_bytes(array)
 
 
-
 def array_to_bytes(input_array: numpy.ndarray) -> bytes:
-    allbytes = bytearray()
-    for strip in range (7):
-        for column in range (128):
+    all_bytes = bytearray()
+    for strip in range(7):
+        for column in range(128):
             bits = []
-            for subrow in range (8):
-                row = strip*8+7-subrow
+            for sub_row in range(8):
+                row = strip * 8 + 7 - sub_row
                 pixel = input_array[row][column]
-                bits.append (pixel)
-            currentbyte = bits_to_byte(bits)
-            print (currentbyte)
-            allbytes.append(currentbyte)
-    return allbytes
+                bits.append(pixel)
+            current_byte = bits_to_byte(bits)
+            if DEBUG_EVERY_BYTE:
+                log.debug(current_byte)
+            all_bytes.append(current_byte)
+    return all_bytes
+
 
 def bits_to_byte(bits: [int]) -> int:
     result = 0
     for bit in bits:
-        result = result<<1 | bit
+        result = result << 1 | bit
     return int(result)
